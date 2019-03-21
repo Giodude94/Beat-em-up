0a11eaa3f0169279cb523967a561d2b73b1937ac
Giodude94 <Giovanni_vega94@yahoo.com>
2019-03-21 18:52:36 -0500

    The first version of the game.

---
+++ Assets/Scripts/WaveController.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WaveController : MonoBehaviour {
+
+    public float dirX;
+    float moveSpeed;
+    bool catPlayerFacingRight;
+
+    void Awake()
+    {
+        catPlayerFacingRight = GameObject.FindGameObjectWithTag("Player").GetComponent<CatController>().facingRight;
+    }
+    // Use this for initialization
+    void Start () {
+        moveSpeed = 5;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+
+
+        //dirX = Input.GetAxisRaw("Horizontal") * moveSpeed * Time.deltaTime;
+        if (catPlayerFacingRight)
+        {
+            transform.position = new Vector2(transform.position.x + dirX, transform.position.y);    
+        }
+        else
+        {
+            transform.position = new Vector2(transform.position.x - dirX, transform.position.y);
+        }
+
+    }
+}
